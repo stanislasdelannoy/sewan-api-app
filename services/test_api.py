a0dace486c311875c1027298efcf9464deaff550
Stanislas Delannoy <stanislas.delannoy@outlook.fr>
2025-05-13 15:21:09 +0200

    Get firsts bills for a customer

--- services/test_api.py
+++ services/test_api.py
@@ -3,4 +3,4 @@ from services.sewan_api import demo_search, demo_get_all_customers, get_a_bill
 if __name__ == "__main__":
     print("Testing demo_search function...")
     # demo_search()
-    get_a_bill()
+    demo_search()
