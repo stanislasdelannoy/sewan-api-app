48eba61cd7908783863f79a8549d6af54cb149e3
Stanislas Delannoy <stanislas.delannoy@outlook.fr>
2025-05-23 11:21:56 +0200

    First version of the front

---
+++ services/get_info.py
@@ -0,0 +1,59 @@
+import requests
+import pandas as pd
+import sys
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_URL = os.getenv("API_URL")
+USER_AGENT = os.getenv("USER_AGENT")
+TOKEN_API = os.getenv("TOKEN_API")
+
+
+headers = {
+    'Authorization': f'bearer {TOKEN_API}',
+    'User-Agent': USER_AGENT,
+    'Accept-Language': 'fr_FR'
+}
+
+def demo_search():
+
+    print("Search (sophia.service.search.search_unified())... ")
+
+    text_search = "Julien DELOBEL"
+    url = (
+        f"{API_URL}?service=sophia.service.Search&method=search_unified&text={text_search}"
+    )
+    print(f"    * URL: {url}")
+
+    response_search = requests.get(url, headers=headers, timeout=10)
+    print(f"    * HTTP response: {response_search}")
+
+    if response_search.status_code != 200:
+        sys.stderr.write(f"  => HTTP error {response_search.status_code} on search.\n")
+        sys.stderr.write(f"  => Body {response_search.content}\n")
+        sys.exit(2)
+    rjson = response_search.json()
+    print(f"    * Webservice response: {rjson}")
+
+    code = int(rjson["code"])
+    if code < 200 or code >= 300:
+        sys.stderr.write(
+            f"  => Search failed with code {rjson['code']} : {rjson['msg']}\n"
+            "(see Webservice response for more details)\n"
+        )
+        sys.exit(2)
+
+    results_array = rjson["result_object1"] or []
+    results_array_str = [
+        f"      * {item['id']} - {item['label']}" for item in results_array
+    ]
+    results_str = "\n".join(results_array_str)
+    print(f"  => Search succeeded. {len(results_array)} item(s) found :\n{results_str}")
+
+
+if __name__ == "__main__":
+    demo_search()
+    
--- streamlit_frontend/pages/login.py
+++ streamlit_frontend/pages/login.py
@@ -14,9 +14,13 @@ def login_page():
 
     # Bouton de connexion
     if st.button("Se connecter"):
-        # Récupérer les valeurs depuis le fichier .env
-        stored_user_agent = os.getenv("USER_AGENT")
-        stored_token_api = os.getenv("TOKEN_API")
+        # Récupérer les valeurs depuis le fichier .env et supprimer les espaces inutiles
+        stored_user_agent = os.getenv("USER_AGENT", "").strip()
+        stored_token_api = os.getenv("TOKEN_API", "").strip()
+
+        # Supprimer les espaces inutiles dans les entrées utilisateur
+        username = username.strip()
+        password = password.strip()
 
         # Vérifier si les champs correspondent aux valeurs stockées
         if username == stored_user_agent and password == stored_token_api:
@@ -24,6 +28,10 @@ def login_page():
             st.success("Connexion réussie ! Redirection vers le tableau de bord...")
         else:
             st.error("Nom d'utilisateur ou mot de passe incorrect.")
+            st.write(f"Stored User Agent: {stored_user_agent}")
+            st.write(f"Stored Token API: {stored_token_api}")
+            st.write(f"Input User Agent: {username}")
+            st.write(f"Input Token API: {password}")
 
     # Bouton pour accéder à la page d'inscription
     if st.button("Pas encore inscrit ? Cliquez ici pour vous inscrire."):
